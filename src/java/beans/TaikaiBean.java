a50801afe9fadf180a09f4a4886fb50a41bcbbc1
s20162057 <s20162057@HCS23202.stdlan.hcs.local>
2018-07-11 12:18:30 +0900

    ログイン

--- src/java/beans/TaikaiBean.java
+++ src/java/beans/TaikaiBean.java
@@ -7,7 +7,43 @@ import javax.inject.Named;
 @RequestScoped
 public class TaikaiBean {
     
+    private boolean mail;
+    private String name1 = "おおもりぴえろ";
+    private String name2 = "大森ピエロ";
+    
     public String next(){
         return "templates/taikai.xhtml";
     }
+    
+    public void clear() {
+        name1 = null;
+        name2 = null;
+    }
+
+    public boolean isMail() {
+        return mail;
+    }
+
+    public void setMail(boolean mail) {
+        this.mail = mail;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+    
+    
+
 }
