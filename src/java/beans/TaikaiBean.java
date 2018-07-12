2442064553492bd254dfbf5e2bad8840c545d64b
s20162057 <s20162057@HCS23202.stdlan.hcs.local>
2018-07-12 09:36:58 +0900

    ログイン

--- src/java/beans/TaikaiBean.java
+++ src/java/beans/TaikaiBean.java
@@ -1,31 +1,51 @@
 package beans;
 
+import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
-public class TaikaiBean {
+public class TaikaiBean implements Serializable{
     
+   
+    private String no = "114514";
+    private String name1 = "おおもりちな";
+    private String name2 = "大森ちな";
     private boolean mail;
-    private String name1 = "おおもりぴえろ";
-    private String name2 = "大森ピエロ";
+    
+    public TaikaiBean() {}
+    
+    public TaikaiBean(String no, String name1, String name2) {
+        this.no = no;
+        this.name1 = name1;
+        this.name2 = name2;
+    }
     
     public String next(){
-        return "templates/taikai.xhtml";
+        return "/templates/taikai.xhtml";
     }
     
     public void clear() {
+        no = null;
         name1 = null;
         name2 = null;
     }
-
+    
     public boolean isMail() {
         return mail;
     }
 
     public void setMail(boolean mail) {
         this.mail = mail;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
     }
 
     public String getName1() {
