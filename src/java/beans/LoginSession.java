75c3139e9fcd1e03b79b67b13fe7587745e13173
s20163025 <s20163025@HCS23201.stdlan.hcs.local>
2018-07-12 14:37:50 +0900

    Merge origin/master
    Conflicts:
    	src/java/beans/HenkyakuBb.java
    	src/java/beans/HenkyakuBean.java
    	src/java/beans/TaikaiBean.java
    	web/resources/css/base.css
    	web/templates/sidebar.xhtml
    	web/templates/taikai.xhtml

--- src/java/beans/LoginSession.java
+++ src/java/beans/LoginSession.java
@@ -3,7 +3,6 @@ package beans;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 @Named
 @SessionScoped
