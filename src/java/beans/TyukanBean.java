e393e811a3882b3af5106c0f2c5d9aab7b451bfe
s20162057 <s20162057@HCS23202.stdlan.hcs.local>
2018-07-17 11:58:17 +0900

    Merge origin/master
    Conflicts:
    	src/java/beans/HenkyakuBb.java
    	web/resources/css/base.css

--- src/java/beans/TyukanBean.java
+++ src/java/beans/TyukanBean.java
@@ -1,7 +1,23 @@
 package beans;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
+@Named
+@RequestScoped
 public class TyukanBean {
     
+    private String money1;
+    private String money2;
+    private String money3;
+    private String money4;
+    private String money5;
+    private String money6;
+    private String money7;
+    private String money8;
+    
+    public void clear() {
+        
+    }
     
 }
